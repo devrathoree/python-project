c7ed0066afa404d03c8aa0d77c680e639d27ba57
devrathoree <120649368+devrathoree@users.noreply.github.com>
2025-03-13 15:11:23 +0530

    Update push.groovy

--- vars/push.groovy
+++ vars/push.groovy
@@ -2,6 +2,6 @@ def call(String IMAGE_NAME , String TAG ){
     withCredentials([usernamePassword(credentialsId: 'docker_creds' , usernameVariable: 'docker_user' , passwordVariable: 'docker_token')]){
         sh" docker login -u ${docker_user} -p ${docker_token} "
     }
-    sh " docker tag "${IMAGE_NAME}":"${TAG}" "${docker_user}"/"${IMAGE_NAME}":"${TAG}" "
-    sh " docker push "${docker_user}"/"${IMAGE_NAME}":"${TAG}" "
+    sh " docker tag ${IMAGE_NAME}:${TAG} ${docker_user}/${IMAGE_NAME}:${TAG} "
+    sh " docker push ${docker_user}/${IMAGE_NAME}:${TAG} "
 }
