3d68f4779213a528da6f505481aa05240ab8f597
devrathoree <devendra.r@zecdata.com>
2025-12-04 15:21:10 +0530

    add varaible in app

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello world! how are you'
+    my_var = os.getenv("MY_SECRET_VALUE", "default-value")
+    return f"Hello world! how are you {my_var}"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=5000)
+    app.run(host='0.0.0.0', port=5000)
